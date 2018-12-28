b508b141174130f903f168a475899106ae48bbf5
黄文 <1299028619@qq.com>
2018-12-28 17:35:03 +0800

    sql文件

--- books (5).sql
+++ books (5).sql
@@ -10,7 +10,7 @@ Target Server Type    : MYSQL
 Target Server Version : 80013
 File Encoding         : 65001
 
-Date: 2018-11-12 21:01:07
+Date: 2018-12-26 21:57:28
 */
 
 SET FOREIGN_KEY_CHECKS=0;
@@ -23,18 +23,21 @@ CREATE TABLE `bcomment` (
   `bcid` bigint(20) NOT NULL AUTO_INCREMENT COMMENT '求购信息评论ID',
   `uid` bigint(20) DEFAULT NULL COMMENT '评论用户ID',
   `buyid` bigint(20) DEFAULT NULL COMMENT '求购信息ID',
-  `bctime` time DEFAULT NULL COMMENT '评论时间',
+  `bctime` datetime DEFAULT NULL COMMENT '评论时间',
   `bc_comment` text CHARACTER SET utf8 COLLATE utf8_general_ci COMMENT '评论内容',
   PRIMARY KEY (`bcid`) USING BTREE,
   KEY `bc_uid` (`uid`) USING BTREE,
   KEY `bc_buyid` (`buyid`) USING BTREE,
   CONSTRAINT `bc_buyid` FOREIGN KEY (`buyid`) REFERENCES `buy` (`buyid`) ON DELETE CASCADE ON UPDATE CASCADE,
   CONSTRAINT `bc_uid` FOREIGN KEY (`uid`) REFERENCES `user` (`uid`) ON DELETE CASCADE ON UPDATE CASCADE
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 ROW_FORMAT=DYNAMIC;
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8 ROW_FORMAT=DYNAMIC;
 
 -- ----------------------------
 -- Records of bcomment
 -- ----------------------------
+INSERT INTO `bcomment` VALUES ('1', '2016210001', '1', null, '书上');
+INSERT INTO `bcomment` VALUES ('2', '2', '4', '2018-12-26 04:29:51', 'aaa');
+INSERT INTO `bcomment` VALUES ('3', '2', '4', '2018-12-26 04:56:45', 'fff');
 
 -- ----------------------------
 -- Table structure for book
@@ -47,19 +50,19 @@ CREATE TABLE `book` (
   `bname` varchar(20) CHARACTER SET utf8 COLLATE utf8_general_ci DEFAULT NULL COMMENT '书籍名称',
   `btype` bigint(10) DEFAULT NULL COMMENT '书籍类型',
   `price` float DEFAULT NULL COMMENT '价格',
-  `content` varchar(100) CHARACTER SET utf8 COLLATE utf8_general_ci DEFAULT NULL COMMENT '内容',
-  `cover` binary(1) DEFAULT NULL COMMENT '封面图片',
   PRIMARY KEY (`bid`) USING BTREE,
   KEY `uid` (`uid`) USING BTREE,
   KEY `btype` (`btype`) USING BTREE,
   CONSTRAINT `btype` FOREIGN KEY (`btype`) REFERENCES `btype` (`btid`) ON DELETE CASCADE ON UPDATE CASCADE,
   CONSTRAINT `uid` FOREIGN KEY (`uid`) REFERENCES `user` (`uid`) ON DELETE CASCADE ON UPDATE CASCADE
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8 ROW_FORMAT=DYNAMIC;
+) ENGINE=InnoDB AUTO_INCREMENT=17 DEFAULT CHARSET=utf8 ROW_FORMAT=DYNAMIC;
 
 -- ----------------------------
 -- Records of book
 -- ----------------------------
-INSERT INTO `book` VALUES ('1', '刘伟', '2016210002', '设计模式', '1', '10', null, null);
+INSERT INTO `book` VALUES ('2', '张三', '2016210002', '大学书法', '1', '5');
+INSERT INTO `book` VALUES ('3', '李四', '2016210002', '高等数学', '1', '2');
+INSERT INTO `book` VALUES ('14', '刘铭', '2', '编译原理', '1', '2');
 
 -- ----------------------------
 -- Table structure for btype
@@ -92,15 +95,20 @@ CREATE TABLE `buy` (
   `uid` bigint(20) DEFAULT NULL COMMENT '用户ID',
   `btitle` varchar(20) CHARACTER SET utf8 COLLATE utf8_general_ci DEFAULT NULL COMMENT '求购信息标题（书籍名）',
   `bcontent` varchar(100) CHARACTER SET utf8 COLLATE utf8_general_ci DEFAULT NULL COMMENT '求购内容',
-  `bdate` time DEFAULT NULL COMMENT '求购日期',
+  `bdate` datetime DEFAULT NULL COMMENT '求购日期',
   PRIMARY KEY (`buyid`) USING BTREE,
   KEY `buid` (`uid`) USING BTREE,
   CONSTRAINT `buid` FOREIGN KEY (`uid`) REFERENCES `user` (`uid`) ON DELETE CASCADE ON UPDATE CASCADE
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 ROW_FORMAT=DYNAMIC;
+) ENGINE=InnoDB AUTO_INCREMENT=16 DEFAULT CHARSET=utf8 ROW_FORMAT=DYNAMIC;
 
 -- ----------------------------
 -- Records of buy
 -- ----------------------------
+INSERT INTO `buy` VALUES ('1', '2016210001', '书籍', '书籍', '2018-12-26 02:28:12');
+INSERT INTO `buy` VALUES ('2', '2016210003', '设计模式', '设计模式 张三', '2018-12-26 02:28:14');
+INSERT INTO `buy` VALUES ('3', '2016210003', '设计模式', '李四版', '2018-12-26 02:28:17');
+INSERT INTO `buy` VALUES ('4', '2016210001', 'grf', 'fdvd', '2018-12-26 02:28:20');
+INSERT INTO `buy` VALUES ('15', '2', '软件工程', '求软件工程', '2018-12-26 13:33:25');
 
 -- ----------------------------
 -- Table structure for news
@@ -114,7 +122,7 @@ CREATE TABLE `news` (
   PRIMARY KEY (`nid`) USING BTREE,
   KEY `news_uid` (`uid`) USING BTREE,
   CONSTRAINT `news_uid` FOREIGN KEY (`uid`) REFERENCES `user` (`uid`) ON DELETE CASCADE ON UPDATE CASCADE
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8 ROW_FORMAT=DYNAMIC;
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8 ROW_FORMAT=DYNAMIC;
 
 -- ----------------------------
 -- Records of news
@@ -136,12 +144,16 @@ CREATE TABLE `report` (
   KEY `bid` (`bid`) USING BTREE,
   CONSTRAINT `aid` FOREIGN KEY (`aid`) REFERENCES `user` (`uid`) ON DELETE CASCADE ON UPDATE CASCADE,
   CONSTRAINT `bid` FOREIGN KEY (`bid`) REFERENCES `user` (`uid`) ON DELETE CASCADE ON UPDATE CASCADE
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8 ROW_FORMAT=DYNAMIC;
+) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=utf8 ROW_FORMAT=DYNAMIC;
 
 -- ----------------------------
 -- Records of report
 -- ----------------------------
-INSERT INTO `report` VALUES ('1', '2016210002', '2016210003', null, '1');
+INSERT INTO `report` VALUES ('1', '2016210002', '2016210003', '人身攻击', '1');
+INSERT INTO `report` VALUES ('2', '2016210003', '201620', '不实信息', '1');
+INSERT INTO `report` VALUES ('3', '2016210001', '2016210002', '不实信息', '0');
+INSERT INTO `report` VALUES ('8', '2', '2', '低俗', '1');
+INSERT INTO `report` VALUES ('9', '2', '2016210002', '诈骗', '1');
 
 -- ----------------------------
 -- Table structure for scomment
@@ -158,12 +170,14 @@ CREATE TABLE `scomment` (
   KEY `csid` (`sid`) USING BTREE,
   CONSTRAINT `cuid` FOREIGN KEY (`uid`) REFERENCES `user` (`uid`) ON DELETE CASCADE ON UPDATE CASCADE,
   CONSTRAINT `scid` FOREIGN KEY (`sid`) REFERENCES `sell` (`sid`) ON DELETE CASCADE ON UPDATE CASCADE
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8 ROW_FORMAT=DYNAMIC;
+) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=utf8 ROW_FORMAT=DYNAMIC;
 
 -- ----------------------------
 -- Records of scomment
 -- ----------------------------
-INSERT INTO `scomment` VALUES ('1', '2016210003', '1', '2018-11-12 17:53:04', null);
+INSERT INTO `scomment` VALUES ('2', '2016210001', '2', '2018-12-25 22:01:35', '求个联系方式');
+INSERT INTO `scomment` VALUES ('7', '2', '21', '2018-12-26 13:37:06', '哈哈哈');
+INSERT INTO `scomment` VALUES ('8', '2', '2', '2018-12-26 14:51:02', '6666');
 
 -- ----------------------------
 -- Table structure for sell
@@ -181,12 +195,14 @@ CREATE TABLE `sell` (
   KEY `sbid` (`bid`) USING BTREE,
   CONSTRAINT `sbid` FOREIGN KEY (`bid`) REFERENCES `book` (`bid`) ON DELETE CASCADE ON UPDATE CASCADE,
   CONSTRAINT `suid` FOREIGN KEY (`uid`) REFERENCES `user` (`uid`) ON DELETE CASCADE ON UPDATE CASCADE
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8 ROW_FORMAT=DYNAMIC;
+) ENGINE=InnoDB AUTO_INCREMENT=24 DEFAULT CHARSET=utf8 ROW_FORMAT=DYNAMIC;
 
 -- ----------------------------
 -- Records of sell
 -- ----------------------------
-INSERT INTO `sell` VALUES ('1', '2016210002', '1', '出售《设计模式》', null, '2018-11-12 00:00:00');
+INSERT INTO `sell` VALUES ('2', '2016210002', '2', '大学书法', '华中师范大学出版 习字与书法课专用教材', '2018-12-12 14:18:52');
+INSERT INTO `sell` VALUES ('3', '2016210003', '3', '低价出售', '同济大学出版社 第七版  上下册', '2018-12-12 14:19:12');
+INSERT INTO `sell` VALUES ('21', '2', '14', '出售编译原理', '                                        出书啦\r\n', '2018-12-26 13:24:09');
 
 -- ----------------------------
 -- Table structure for user
@@ -196,15 +212,19 @@ CREATE TABLE `user` (
   `uid` bigint(20) NOT NULL COMMENT '用户ID',
   `tname` varchar(10) CHARACTER SET utf8 COLLATE utf8_general_ci DEFAULT NULL COMMENT '真实姓名',
   `uname` varchar(10) CHARACTER SET utf8 COLLATE utf8_general_ci DEFAULT NULL COMMENT '用户名',
-  `upass` varchar(10) CHARACTER SET utf8 COLLATE utf8_general_ci DEFAULT NULL COMMENT '密码',
+  `upass` varchar(10) CHARACTER SET utf8 COLLATE utf8_general_ci DEFAULT '' COMMENT '密码',
   `utype` int(11) DEFAULT '0' COMMENT '管理员1  用户0',
   `ustate` bigint(10) DEFAULT '0' COMMENT '默认0 冻结状态1',
+  `uclass` bigint(10) DEFAULT NULL COMMENT '用户等级积分',
   PRIMARY KEY (`uid`) USING BTREE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 ROW_FORMAT=DYNAMIC;
 
 -- ----------------------------
 -- Records of user
 -- ----------------------------
-INSERT INTO `user` VALUES ('2016210001', '张三', 'admin', 'admin', '1', '0');
-INSERT INTO `user` VALUES ('2016210002', '李四', 'user1', 'user1', '0', '0');
-INSERT INTO `user` VALUES ('2016210003', '王五', 'user2', 'user2', '0', '1');
+INSERT INTO `user` VALUES ('1', '1', '1', '1', '1', '0', null);
+INSERT INTO `user` VALUES ('2', '2', '2', '2', '0', '0', '47');
+INSERT INTO `user` VALUES ('201620', 'sa', 'sa', '1', '0', '0', '0');
+INSERT INTO `user` VALUES ('2016210001', '张三', 'admin', 'admin', '1', '0', '27');
+INSERT INTO `user` VALUES ('2016210002', '李四', '张二', 'user1', '0', '0', '0');
+INSERT INTO `user` VALUES ('2016210003', '王五', 'user2', 'user2', '0', '1', '0');
